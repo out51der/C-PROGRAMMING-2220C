7a2061e9d52f7bf40c9ce6c6e690d7b44d7be704
out51der <30524888+out51der@users.noreply.github.com>
2021-06-05 17:27:41 -0400

    Create 5.4.2 Iterating through an array example.c
    Iterating through an array example: Program that computes the sum of an array's elements.

---
+++ 5.4.2 Iterating through an array example.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+   const int NUM_ELEMENTS = 8; // Number of elements
+   int userVals[NUM_ELEMENTS]; // User numbers
+   int i;                      // Loop index
+   int sumVal;                 // For computing sum
+   
+   // Prompt user to populate array
+   printf("Enter %d integer values...\n", NUM_ELEMENTS);
+   
+   for (i = 0; i < NUM_ELEMENTS; ++i) {
+      printf("Value: ");
+      scanf("%d", &(userVals[i]));
+   }
+   
+   // Determine sum
+   sumVal = 0;
+   for (i = 0; i < NUM_ELEMENTS; ++i) {
+      sumVal = sumVal + userVals[i];
+   }
+   
+   printf("Sum: %d\n", sumVal);
+   
+   return 0;
+}
