e9118985702e95bfd6f0977934c0cdd436dc4088
out51der <30524888+out51der@users.noreply.github.com>
2021-06-06 15:20:19 -0400

    Create 5.9.1: Initializing a two-dimensional array during declaration 2220C.c
    A programmer can initialize a two-dimensional array's elements during declaration using nested braces, as below. Multiple lines make the rows and columns more visible.

---
+++ 5.9.1: Initializing a two-dimensional array during declaration 2220C.c
@@ -0,0 +1,8 @@
+// Initializing a 2D array
+int numVals[2][3] = { {22, 44, 66}, {97, 98, 99} };
+
+// Use multiple lines to make rows more visible
+int numVals[2][3] = {
+   {22, 44, 66}, // Row 0
+   {97, 98, 99}  // Row 1
+};
