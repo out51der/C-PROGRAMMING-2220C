b7116f55406a6a90f8fbe3cfa70278d2b2bddf38
out51der <30524888+out51der@users.noreply.github.com>
2021-06-08 23:01:53 -0400

    Update PGM ImageMoification.c

--- PGM ImageMoification.c
+++ PGM ImageMoification.c
@@ -46,20 +46,6 @@
 //*** ????????????????????????????????
 int theImageArray[MAX_HEIGHT][MAX_WIDTH];
 
-for (int row = 0; row < MAX_HEIGHT; row++) {
-
-		for (int col = 0; col < MAX_WIDTH; col++) {
-
-			if (theImageArray[row][col] < 127) {
-			theImageArray[row][col] = 0;
-			}
-			if (theImageArray[row][col] > 127) {
-			theImageArray[row][col] = 255;
-			}
-
-		}
-	}
-
 
 
 // You will need an identical  array to use to hold
@@ -238,7 +224,19 @@ int main(void)
 	//****
 	//**** Your code to transform the image goes here ...
 	//****
+	for (int i = 0; i < MAX_HEIGHT; i++) {
 
+		for (int j = 0; j < MAX_WIDTH; j++) {
+
+			if (theImageArray[i][j] < 127) {
+				theImageArray[i][j] = 0;
+			}
+			if (theImageArray[i][j] > 127) {
+				theImageArray[i][j] = 255;
+			}
+
+		}
+	}
 	//Transfer the current values in theImageArray back to disk
 	write_pgm_file_from_array(pgmOutFileName, theImageArray, "# JR test file", height, width,
 		maxPixel);
