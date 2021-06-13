58927ef13070c8d85d8a8b138789c787afd7a057
out51der <30524888+out51der@users.noreply.github.com>
2021-06-13 18:59:13 -0400

    Create 6.11.1 Modifying a string parameter.c
    Functions commonly modify strings. The following function modifies a string by replacing spaces with hyphens.
    
    OUTPUT:
    
    Enter string with spaces: 
    Hello there everyone.
    String with hyphens: Hello-there-everyone.
    
    ...
    
    Enter string with spaces: 
    Good bye  now   !!!
    String with hyphens: Good-bye--now---!!!

---
+++ 6.11.1 Modifying a string parameter.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <string.h>
+
+// Function replaces spaces with hyphens
+void StrSpaceToHyphen(char modString[]) {
+   int i;  // Loop index
+   
+   for (i = 0; i < strlen(modString); ++i) {
+      if (modString[i] == ' ') {
+         modString[i] = '-';
+      }
+   }
+   
+}
+
+int main(void) {
+   const int INPUT_STR_SIZE = 50;  // Input string size
+   char userStr[INPUT_STR_SIZE];   // Input string from user
+   
+   // Prompt user for input
+   printf("Enter string with spaces: \n");
+   fgets(userStr, INPUT_STR_SIZE, stdin);
+
+   // Call function to modify user defined string
+   StrSpaceToHyphen(userStr);
+   
+   printf("String with hyphens: %s\n", userStr);
+   
+   return 0;
+}
