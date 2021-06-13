c18ec58e9e820c12a0ddb76dafaa683b7cabf6d2
out51der <30524888+out51der@users.noreply.github.com>
2021-06-13 16:18:01 -0400

    Update 6.9.1 Calculating monetary change.c
    #include <stdio.h>
    
    typedef struct MonetaryChange_struct {
       int dollars;
        int quarters;
        int dimes;
        int nickels;
    } MonetaryChange;
    
    MonetaryChange ComputeChange(int cents) {
       MonetaryChange change;
       int nickel = 5;    // Each nickel consists of 5 cents.
       int dime = 10;     // Each dime consists of 10 cents.   
       int quarter = 25;     // Each quarter consists of 25 cents.
       int dollar = 100;    // Each dollar consists of 100 cents.
    
       change.dollars=0;
       change.quarters=0;
       change.dimes=0;
       change.nickels=0;
    
    
       do{  
            if (cents >= 100){
                change.dollars = cents / dollar;  
                cents = cents % change.dollars;
            }
            else if (cents >= 25){
                change.quarters = cents / quarter;  
                cents = cents % quarter;
            }
            else if (cents >= 10){
                change.dimes = cents /dime;  
                cents = cents % dime;
            }
            else if (cents >= 5){
                change.nickels = cents /nickel;  
                cents = cents % nickel;
            }
        }while (cents != 0);
    
    
       return change;
    }
    
    int main(void) {
       int userCents = 0;
       MonetaryChange change;
    
       printf("Enter cents: ");
       scanf("%d", &userCents);
    
       change = ComputeChange(userCents);
    
       // printf("Quarters: %d\n", change.quarters);
       printf("Your change is : %d dollar, %d quarters, %d dime, %d nickels\n", change.dollars, change.quarters, change.dimes, change.nickels);
    
       return 0;
    }

--- 6.9.1 Calculating monetary change.c
+++ 6.9.1 Calculating monetary change.c
@@ -35,3 +35,65 @@ int main(void) {
    
    return 0;
 }
+
+
+
+#include <stdio.h>
+
+typedef struct MonetaryChange_struct {
+   int dollars;
+    int quarters;
+    int dimes;
+    int nickels;
+} MonetaryChange;
+
+MonetaryChange ComputeChange(int cents) {
+   MonetaryChange change;
+   int nickel = 5;    // Each nickel consists of 5 cents.
+   int dime = 10;     // Each dime consists of 10 cents.   
+   int quarter = 25;     // Each quarter consists of 25 cents.
+   int dollar = 100;    // Each dollar consists of 100 cents.
+
+   change.dollars=0;
+   change.quarters=0;
+   change.dimes=0;
+   change.nickels=0;
+
+
+   do{  
+        if (cents >= 100){
+            change.dollars = cents / dollar;  
+            cents = cents % change.dollars;
+        }
+        else if (cents >= 25){
+            change.quarters = cents / quarter;  
+            cents = cents % quarter;
+        }
+        else if (cents >= 10){
+            change.dimes = cents /dime;  
+            cents = cents % dime;
+        }
+        else if (cents >= 5){
+            change.nickels = cents /nickel;  
+            cents = cents % nickel;
+        }
+    }while (cents != 0);
+
+
+   return change;
+}
+
+int main(void) {
+   int userCents = 0;
+   MonetaryChange change;
+
+   printf("Enter cents: ");
+   scanf("%d", &userCents);
+
+   change = ComputeChange(userCents);
+
+   // printf("Quarters: %d\n", change.quarters);
+   printf("Your change is : %d dollar, %d quarters, %d dime, %d nickels\n", change.dollars, change.quarters, change.dimes, change.nickels);
+
+   return 0;
+}
