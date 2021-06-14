9ba941489771c6b5f09a6ffeede4371ee543c5bc
out51der <30524888+out51der@users.noreply.github.com>
2021-06-13 23:00:32 -0400

    Create Construct 6.16.1 Header file guards.c
    One last consideration that must be dealt with is that a header file could get included multiple times, causing the compiler to generate errors indicating an item defined in that header file is defined multiple times (the above header files only declared functions and didn't define them, but other header files may define functions, types, constants, and other items). Multiple inclusion commonly can occur when one header file includes another header file, e.g., the main file includes file1.h and file2.h, and file1.h also includes file2.h -- thus, file2.h would get included twice into the main file.
    
    The solution is to add some additional preprocessor directives, known as header file guards, to the .h file as follows.
    
    Construct 6.16.1: Header file guards.
    
    
    Header file guards are preprocessor directives, which cause the compiler to only include the contents of the header file once. #define FILENAME_H defines the symbol FILENAME_H to the preprocessor. The #ifndef FILENAME_H and #endif form a pair that instructs the preprocessor to process the code between the pair only if FILENAME_H is not defined ("ifndef" is short for "if not defined"). Thus, if the preprocessor includes encounter the header more than once, the code in the file during the second and any subsequent encounters will be skipped because FILENAME_H was already defined.
    
    Good practice is to guard every header file. The following shows the threeintsfcts.h file with the guarding code added.

---
+++ Construct 6.16.1 Header file guards.c
@@ -0,0 +1,20 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+
+// Header file contents
+
+#endif
+
+
+
+
+
+//
+
+#ifndef THREEINTSFCTS_H
+#define THREEINTSFCTS_H
+
+int ThreeIntsSum(int num1, int num2, int num3);
+int ThreeIntsAvg(int num1, int num2, int num3);
+
+#endif
