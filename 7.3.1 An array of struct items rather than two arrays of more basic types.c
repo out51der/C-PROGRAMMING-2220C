7e79eee6c14792c42ed60074b673ff1f9cc736b9
out51der <30524888+out51der@users.noreply.github.com>
2021-06-19 19:24:02 -0400

    Update 7.3.1 An array of struct items rather than two arrays of more basic types.c
    #pragma warning(disable : 4996) // Supressed 4996c error 
    Issues with constant Variables

--- 7.3.1 An array of struct items rather than two arrays of more basic types.c
+++ 7.3.1 An array of struct items rather than two arrays of more basic types.c
@@ -1,3 +1,4 @@
+#pragma warning(disable : 4996)
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -5,42 +6,55 @@
 const int MAX_COUNTRY_NAME_LENGTH = 50;
 
 typedef struct CountryTvWatch_struct {
-   char countryName[MAX_COUNTRY_NAME_LENGTH];
-   int tvMinutes;
+	char countryName[50];
+	int tvMinutes;
 } CountryTvWatch;
 
+void PrintCountryNames(CountryTvWatch ctryList[], int numCountries)
+{
+	int i;
+	for (i = 0; i < numCountries; ++i) {
+		printf(" %s \n ", ctryList[i].countryName);
+	}
+}
+
 int main(void) {
-   // Source: www.statista.com, 2010
-   const int NUM_COUNTRIES = 4;
-   
-   CountryTvWatch countryList[NUM_COUNTRIES];
-   char countryToFind[MAX_COUNTRY_NAME_LENGTH];
-   bool countryFound;
-   int i;
-   
-   strcpy(countryList[0].countryName, "Brazil");
-   countryList[0].tvMinutes = 222;
-   strcpy(countryList[1].countryName, "India");
-   countryList[1].tvMinutes = 119;
-   strcpy(countryList[2].countryName, "U.K.");
-   countryList[2].tvMinutes = 242;
-   strcpy(countryList[3].countryName, "U.S.A.");
-   countryList[3].tvMinutes = 283;
-   
-   printf("Enter country name: ");
-   scanf("%s", countryToFind);
-  
-   countryFound = false;
-   for (i = 0; i < NUM_COUNTRIES && !countryFound; ++i) { // Find country's index
-      if (strcmp(countryList[i].countryName, countryToFind) == 0) {
-         countryFound = true;
-         printf("People in %s watch\n", countryToFind);
-         printf("%d minutes of TV daily.\n", countryList[i].tvMinutes);
-      }
-   }
-   if (!countryFound) {
-      printf("Country not found, try again.\n");
-   }
-   
-   return 0;
+	// Source: www.statista.com, 2010
+	const int NUM_COUNTRIES = 4;
+
+	CountryTvWatch countryList[4];
+	char countryToFind[50];
+	bool countryFound;
+	int i;
+
+	strcpy(countryList[0].countryName, "Brazil");
+	countryList[0].tvMinutes = 222;
+	strcpy(countryList[1].countryName, "India");
+	countryList[1].tvMinutes = 119;
+	strcpy(countryList[2].countryName, "U.K.");
+	countryList[2].tvMinutes = 242;
+	strcpy(countryList[3].countryName, "U.S.A.");
+	countryList[3].tvMinutes = 283;
+
+	printf("Enter country name: \n");
+	scanf("%s", countryToFind);
+
+	countryFound = false;
+	for (i = 0; i < NUM_COUNTRIES && !countryFound; ++i) { // Find country's index
+		if (strcmp(countryList[i].countryName, countryToFind) == 0) {
+			countryFound = true;
+			printf("People in %s watch\n", countryToFind);
+			printf("%d minutes of TV daily.\n", countryList[i].tvMinutes);
+		}
+	}
+	if (!countryFound) {
+		printf("Country not found, try again.\n");
+		printf("Valid countries:\n");
+		PrintCountryNames(countryList, NUM_COUNTRIES);
+	}
+
+	system("pause");
+
+
+	return 0;
 }
