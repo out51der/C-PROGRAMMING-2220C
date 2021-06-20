dea5624456ac2a6b3e2521ba2d726903a0184772
out51der <30524888+out51der@users.noreply.github.com>
2021-06-19 20:15:25 -0400

    Update 7.3.2.2 Pizzas in store calories Structs and arrays.c
     To assign the first value from input to Barbecue's data member caloriesPerSlice use pizzasInStore[0] to get the struct at element 0, which corresponds to Barbecue. Then, use the dot operator to access the data member caloriesPerSlice. Ham and Cheese's caloriesPerSlice is similarly assigned with the next value from input by getting the struct at element 1, and then using the dot operator to access the data member caloriesPerSlice. Ex: scanf("%d", &pizzasInStore[0].caloriesPerSlice)

--- 7.3.2.2 Pizzas in store calories Structs and arrays.c
+++ 7.3.2.2 Pizzas in store calories Structs and arrays.c
@@ -11,7 +11,15 @@ int main(void) {
 
    strcpy(pizzasInStore[0].pizzaName, "Barbecue");
    strcpy(pizzasInStore[1].pizzaName, "Ham and Cheese");
-/* Your code goes here */
+/* Your code goes here 
+ To assign the first value from input to Barbecue's data member caloriesPerSlice use pizzasInStore[0] 
+ to get the struct at element 0, which corresponds 
+ to Barbecue. Then, use the dot operator to access the data member 
+ caloriesPerSlice. Ham and Cheese's caloriesPerSlice is similarly 
+ assigned with the next value from input by getting the struct at element 1, 
+ and then using the dot operator to access the data member caloriesPerSlice. 
+ Ex: scanf("%d", &pizzasInStore[0].caloriesPerSlice)
+*/
 
 
    pizzasInStore[0].caloriesPerSlice = 0;
