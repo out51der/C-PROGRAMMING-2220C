4a2a3d9df4244b3345161ca535bf26dff6865af0
out51der <30524888+out51der@users.noreply.github.com>
2021-07-07 23:00:01 -0400

    Create 9.2.3 Output formatting: Printing a maximum number of digits in the fraction.c
    Write a single statement that prints outsideTemperature with 2 digits in the fraction (after the decimal point). End with a newline. Sample output with input 103.45632:
    103.46
    
    OUTPUT:
    Testing with input: 103.45632
    Your output
    103.46
    Testing with input: 70.1234
    Your output
    70.12

---
+++ 9.2.3 Output formatting: Printing a maximum number of digits in the fraction.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+   double outsideTemperature;
+
+   scanf("%lf", &outsideTemperature);
+
+   /* Your solution goes here  */
+   printf("%.2lf\n", outsideTemperature);
+
+   return 0;
+}
