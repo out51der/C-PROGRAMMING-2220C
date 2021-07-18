c6b2b0e035ea7a4d10d5c5b70bb4f565fd114be5
out51der <30524888+out51der@users.noreply.github.com>
2021-07-17 22:31:42 -0400

    Create 10.3.1.3 Enter the output of binary search.c
    The input exists alternatively in the lower half or upper half of the range during the recursive search until the base case is reached.
     
    When number equals midVal (base case), " a" is printed.
     
    When number is less than midVal (recursive case), " b" is printed, and a recursive call searches the lower half, from lowVal to midVal.
     
    When number is greater than or equal to midVal (recursive case), " c" is printed, and a recursive call searches the upper half, from midVal + 1 to highVal.
     
    That is, with input 3:
    For range [0, 8], 3 is in the lower half of the range.
    For range [0, 4], 3 is in the upper half of the range.
    
    Recursive function call outputs:
    findNumber(3, 0, 8) prints: 3 4 b
    findNumber(3, 0, 4) prints: 3 2 c
    findNumber(3, 3, 4) prints: 3 3 a
    Yours	3 4 b
    3 2 c
    3 3 a
    Expected	
    3 4 b
    3 2 c
    3 3 a

---
+++ 10.3.1.3 Enter the output of binary search.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <string.h>
+
+void FindNumber(int number, int lowVal, int highVal) {
+   int midVal;
+
+   midVal = (highVal + lowVal) / 2;
+   printf("%d %d", number, midVal);
+
+   if (number == midVal) {
+      printf(" a\n");
+   }
+   else {
+      if (number < midVal) {
+         printf(" b\n");
+         FindNumber(number, lowVal, midVal);
+      }
+      else {
+         printf(" c\n");
+         FindNumber(number, midVal + 1, highVal);
+      }
+   }
+}
+
+int main() {
+   int number;
+
+   scanf("%d", &number);
+   FindNumber(number, 0, 8);
+   
+   return 0;
+}
+
+
